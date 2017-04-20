60632f41885b302c5b689b5184eb8ce58af3947d
kiwi <5103662780@163.com>
2017-04-20 11:22:34 +0800

    加入创建数据库和表的各项配置。。

---
+++ app/src/main/java/db/City.java
@@ -0,0 +1,38 @@
+package db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Kiwi7 on 2017/4/20.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCityName() {
+        return cityName;
+    }
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+    public int getCityCode() {
+        return cityCode;
+    }
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+    public int getProvinceId() {
+        return provinceId;
+    }
+    public void setPrivinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
